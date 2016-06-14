e9a0f1842c348681af78113a865ba31ec2eb3b08
Cirych <cir_reg@mail.ru>
2016-06-14 21:03:57 +0300

    svg style manipulation

--- js/main.js
+++ js/main.js
@@ -2,14 +2,51 @@
 */
 'use strict';
 
+
+
 function isReady(f){/in/.test(document.readyState)?setTimeout('isReady('+f+')',9):f()};
 
 isReady(function(){
-    document
-        .getElementById("boardImg")
+    // svg elements
+    var boardImg = document
+        .getElementById('boardImg')
         .contentDocument
-        .getElementById("fields")
+
+    // hidden buttons event listener
+    boardImg
+        .getElementById('fields')
         .addEventListener('click', function (event) {
-            console.log(event.srcElement.id);
+            console.log(event.srcElement.id.slice(1));
+            showMove(boardImg, "x", event.srcElement.id.slice(1))
         });
+
+    // new game event listener
+    document
+        .getElementById('newGame')
+        .addEventListener('click', function (event) {
+            reset(boardImg);
+        });
+
 });
+
+function showMove(boardImg, type, index) {
+    boardImg
+        .getElementById(type + index)
+        .classList.add('showMove');
+};
+
+function reset(boardImg) {
+    Array.prototype.slice.call(
+        boardImg
+        .getElementById('tictacs')
+        .children
+    ).concat(
+    Array.prototype.slice.call(
+        boardImg
+        .getElementById('wins')
+        .children
+    ))
+        .forEach(function (item) {
+            item.classList.remove('showMove');
+        });
+};
