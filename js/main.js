c42522562ec790c1aae1c86c0ce7cf4dfc899ffb
Cirych <cir_reg@mail.ru>
2016-06-14 18:11:32 +0300

    event listeners to svg hidden buttons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+/*! Tic Tac Toe Game v1.0.0 | (c) 2016 Cirych. | ki-tec.ru
+*/
+'use strict';
 
+function isReady(f){/in/.test(document.readyState)?setTimeout('isReady('+f+')',9):f()};
+
+isReady(function(){
+    document
+        .getElementById("boardImg")
+        .contentDocument
+        .getElementById("fields")
+        .addEventListener('click', function (event) {
+            console.log(event.srcElement.id);
+        });
+});
